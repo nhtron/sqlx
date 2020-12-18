0c932ab684da06821dbdd3c58a3d22d31d211925
nhtron <73663644+nhtron@users.noreply.github.com>
2020-12-18 18:31:43 +0700

    Update sqlx.go
    adds support for structscan in multiple resultset queries

--- sqlx.go
+++ sqlx.go
@@ -595,6 +595,14 @@ func (r *Rows) MapScan(dest map[string]interface{}) error {
 	return MapScan(r, dest)
 }
 
+func (r *Rows) NextResultSet() bool {
+	if !r.Rows.NextResultSet() {
+		return false
+	}
+	// reset fields cache
+	r.started = false
+	return true
+}
 
 // StructScan is like sql.Rows.Scan, but scans a single Row into a single Struct.
 // Use this and iterate over Rows manually when the memory load of Select() might be
